cacdf62869aa23e2b285aeebd18b4f8a83ebd9e1
JianZJ-75 <2279814184@qq.com>
2026-04-10 16:53:43 +0800

    迭代 3：艾宾浩斯复习调度算法 后端已完成

---
+++ backend/src/main/java/com/jianzj/mistake/hub/backend/service/ReviewRecordService.java
@@ -0,0 +1,24 @@
+package com.jianzj.mistake.hub.backend.service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jianzj.mistake.hub.backend.entity.ReviewRecord;
+import com.jianzj.mistake.hub.backend.mapper.ReviewRecordMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 复习记录 服务类
+ * </p>
+ *
+ * @author jian.zhong
+ * @since 2026-04-09
+ */
+@Service
+@Slf4j
+public class ReviewRecordService extends ServiceImpl<ReviewRecordMapper, ReviewRecord> {
+
+    // ===== 业务方法 =====
+
+    // ===== 工具方法 =====
+}
